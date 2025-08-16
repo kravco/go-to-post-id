8bea856519a49a882b06cb61c89a27d28b5b527f
Matej Kravjar <115938+kravco@users.noreply.github.com>
2025-08-16 13:55:46 +0200

    chore: refactor plugin code

--- go-to-post-id.php
+++ go-to-post-id.php
@@ -18,35 +18,35 @@
  * Domain Path: /languages
  */
 
-// protect against direct access.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
+// @phpstan-ignore smaller.alwaysFalse
+if ( PHP_VERSION_ID < 70400 ) {
+	define( 'GO_TO_POST_ID_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+	require __DIR__ . '/src/error-handling/unsupported-php-version.php';
+	return;
+}
 
 add_action(
 	'admin_bar_menu',
 	function ( $wp_admin_bar ) {
-		if ( current_user_can( 'edit_posts' ) ) {
-			/**
-			 * Note that we do not use PHP type here, because admin bar class is filterable.
-			 *
-			 * @phpstan-var WP_Admin_Bar $wp_admin_bar
-			 */
-			$wp_admin_bar->add_node(
-				[
-					'id'    => 'go-to-post-id',
-					'title' => '<form action="' . esc_attr( admin_url( 'post.php' ) ) . '">'
-						. '<input type="text" name="post" placeholder="'
-						. esc_attr( _x( 'Go to Post ID', 'input-placeholder', 'go-to-post-id' ) )
-						. '" style="padding: 0 4px; min-height: 24px; height: 24px; width: '
-						. esc_attr( _x( '80', 'input-width-in-px', 'go-to-post-id' ) )
-						. 'px" />'
-						. '<input type="hidden" name="action" value="edit" />'
-						. '</form>',
-				]
-			);
+		if ( ! current_user_can( 'edit_posts' ) ) {
+			return;
 		}
+		// @phpstan-ignore method.nonObject
+		$wp_admin_bar->add_node(
+			[
+				'id'    => 'go-to-post-id',
+				'title' => '<form action="' . esc_attr( admin_url( 'post.php' ) ) . '">'
+					. '<input type="text" name="post" placeholder="'
+					. esc_attr( _x( 'Go to Post ID', 'input-placeholder', 'go-to-post-id' ) )
+					. '" style="padding: 0 4px; min-height: 24px; height: 24px; width: '
+					. esc_attr( _x( '80', 'input-width-in-px', 'go-to-post-id' ) )
+					. 'px" />'
+					. '<input type="hidden" name="action" value="edit" />'
+					. '</form>',
+			]
+		);
 	},
 	PHP_INT_MAX
 );
