81cb595e84bb0a97699c774c1a96f887ddd43eed
Matej Kravjar <115938+kravco@users.noreply.github.com>
2025-08-16 21:05:55 +0200

    chore: refactor translation job

--- jobs/extract-translations.sh
+++ jobs/extract-translations.sh
@@ -13,7 +13,8 @@ then
     exit 1
 fi
 
-echo Compiling plugin translations into pot file slug=$slug
+output=languages/$slug.pot
+echo Compiling plugin translations into pot file: $output
 mkdir -p languages
-vendor/bin/wp i18n make-pot . languages/$slug.pot \
+vendor/bin/wp i18n make-pot . ./$output \
   --include=$( jobs/.files.sh | sed -e 's/ /,/g' )
